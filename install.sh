badb8e96cdf7a78eb805802f7f7b0b6cc3dc01dd
Lennart Zellmer <lennartzellmer@Lennarts-MacBook-Pro.local>
2015-12-27 20:18:39 +0100

    Roberts-Version

--- install.sh
+++ install.sh
@@ -42,6 +42,10 @@ brew cask install keka
 brew cask install adobe-creative-cloud
 brew cask install sqlitebrowser
 brew cask install spotify
+brew cask install parallels-desktop
+brew cask install microsoft-office
+brew cask install mamp
+brew cask install minecraft
 
 ## Install Atom
 brew cask install atom
